1dacccc92aa756f6131938e6ba4a1a90090ee1ad
Karol Dargacz <karol.dargacz@gmail.com>
2018-11-15 09:59:37 +0100

    else

--- kolokwium1/src/main/java/pl/shop/javaee/web/AllNewslettersServlet.java
+++ kolokwium1/src/main/java/pl/shop/javaee/web/AllNewslettersServlet.java
@@ -2,12 +2,18 @@ package pl.shop.javaee.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pl.shop.javaee.domain.Newsletter;
+import pl.shop.javaee.service.NewsStorage;
 
 
 @WebServlet("/allnewsletters")
@@ -24,8 +30,15 @@ public class AllNewslettersServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();	
 		
 		
+		ServletContext servletContext = getServletContext();
+		NewsStorage newStorage = (NewsStorage) servletContext.getAttribute("newsletter");
 		
+		Map<String, Newsletter> db = newStorage.getAllNewsletters();
 		
+		Set<Map.Entry<String, Newsletter>> xSet = db.entrySet();
+		for (Map.Entry<String, Newsletter> x : xSet) {
+			out.println(x.getKey() + " " + x.getValue() + "<br/>");
+		}
 		
 		out.close();
 	}
